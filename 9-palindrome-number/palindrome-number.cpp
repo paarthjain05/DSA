b264b76d6ea87778e21d6cdf5cc084b3f69d03a3
Paarth Jain <75124546+paarthjain05@users.noreply.github.com>
2025-01-30 14:35:01 +0530

    Time: 2 ms (52.39%) | Memory: 10.6 MB (23.24%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return 0;
-        }else{
-            long long rev=0;
-            int temp=x;
-            while(temp!=0){
-                int digit=temp%10;
-                rev=rev*10+digit;
-                temp=(temp-digit)/10;
-            }
-            if(rev==x){
-                return 1;
-            }else{
-                return 0;
-            }
+        string s = to_string(x);
+        for(int i = 0; i<s.size()/2; i++){
+            if(s[i] != s[s.size()-i-1])return false;
         }
-    return 0;
+        return true;
     }
 };
