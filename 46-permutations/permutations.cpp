d30340f4b1cf37cfcf5f63a5e4f6736e76186679
Paarth Jain <75124546+paarthjain05@users.noreply.github.com>
2026-03-05 11:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (98.08%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& n) {
+        vector<vector<int>>ans;
+        sort(n.begin(),n.end());
+        do{
+           ans.push_back(n); 
+        }while(next_permutation(n.begin(), n.end()));
+        return ans;
+    }
+};
